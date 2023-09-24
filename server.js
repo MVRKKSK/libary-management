420b1d4e4747822b245e4498d207f8af1877d131
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2023-09-24 20:29:42 +0530

    changed Connection before express listsen

--- server.js
+++ server.js
@@ -24,30 +24,14 @@ app.use(
     })
 );
 
-/* const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-    }
-})
-
-io.on('connection', socket => {
-    console.log('connection made successfully')
-    socket.on('message', payload => {
-        console.log('Message received on server: ', payload)
-        io.emit('message', payload)
-    })
-})
-
-server.listen(7000, () => {
-    console.log('I am listening at port: 7000');
-})
- */
 app.use(bodyParser.json());
 readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)))
 app.get("/", (req, res) => {
     res.send("Unleash The Bankai")
 })
 
-app.listen(process.env.PORT || 8000, (req, res) => {
-    console.log("server is connected")
+db().then(() => {
+    app.listen(process.env.PORT || 8000, (req, res) => {
+        console.log("server is connected")
+    })
 })
